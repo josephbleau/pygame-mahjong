b361fc1ccf832387e4ad3463d77f3fdec058a192
Joseph Bleau <jbleau@gmail.om>
2011-05-19 20:44:22 -0400

    refactored input handling

--- game.py
+++ game.py
@@ -77,13 +77,21 @@ class Game:
     self.sound_on = sound                           # If sound is on
     self.editor = editor                            # If the editor is running
     
-    self.render_func = { 'playing' : self.render_playing,               \
-                         'menu' : self.render_menu,                     \
-                         'level_select' : self.render_level_select,     \
-                         'paused': self.render_paused,                  \
-                         'level_complete' : self.render_level_complete, \
-                         'highscores': self.render_highscores }
+    # State-based render handling
+    self.render_func = { 'playing'        : self.render_playing,         \
+                         'menu'           : self.render_menu,            \
+                         'level_select'   : self.render_level_select,    \
+                         'paused'         : self.render_paused,          \
+                         'level_complete' : self.render_level_complete,  \
+                         'highscores'     : self.render_highscores }
     
+    # State-based input handling
+    self.input_handlers = { 'playing'        : self.handle_playing_input,       \
+                            'menu'           : self.handle_menu_input,          \
+                            'level_select'   : self.handle_level_select_input,  \
+                            'paused'         : self.handle_paused_input,        \
+                            'level_complete' : self.handle_level_complete_input, \
+                            'highscores'     : self.handle_highscores_input }
     # If true then play sound
     if self.sound_on:
       pygame.mixer.music.load('res/bg.mp3')
@@ -115,132 +123,142 @@ class Game:
 
     fh.close()
   
-
-  def handle_tile_click(self,event):
-    "Send mouse click events to us. We handle selected / unselecting tiles, \
-     match-pairing, and whether or not the matches were valid. This function also handles \
-     state-changing when all tiles are missing. "
-    if event.type == pygame.KEYDOWN:
-      return
-    for tile in sorted(self.tiles, key=byTopRight, reverse=True) :
-        x,y = event.pos
-        if x >= tile.x - tile.z * 3 and x <= tile.x + 40 - tile.z * 3 and \
-           y >= tile.y - tile.z * 3 and y <= tile.y + 60 - tile.z * 3:
-           if self.selected == tile: # Selecting the selected tile unselects it.
-            self.selected = None
-            return
-           if self.selected: # We clicked on a tile, is a tile already selected?
-            if self.selected.tileno == tile.tileno:     # Remove tiles if they're the same
-              # But only if both tiles are not blocked on left & right
-              if not tile.is_blocked(self.tiles) and not self.selected.is_blocked(self.tiles):
-                self.tiles.remove(self.selected)
-                self.tiles.remove(tile)
-                self.pieces_removed += 2
-                
-                # If we won!
-                if len(self.tiles) == 0:
-                  self.state = 'level_complete'
-                  self.score = str((pygame.time.get_ticks()-self.time_started)/1000)
-                  pygame.event.clear()
-                  
-                  self.write_score()
-                  return
-              self.selected = None
-              return
-            else:
-              self.selected = None
-              return
-           else:
-            if not tile.is_blocked(self.tiles):
-              self.selected = tile       
-           return
-           
-           
-    self.selected = None
-  
   def handle_input(self, event):
     "Based on the games current state, manage our mouse input."
-    if self.state == 'menu':
-      
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-        if self.m_selector == 3:
-          self.m_selector = 0
-        else:
-          self.m_selector += 1
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-        if self.m_selector == 0:
-          self.m_selector = 3
-        else:
-          self.m_selector -= 1
-          
-      # When the user makes a selection
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-        if self.m_selector == 0:
-          self.state = 'level_select'
-          self.m_selector = 0
-          return
-        if self.m_selector == 3:
-          sys.exit()
-          
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        sys.exit()
-          
-    if self.state == 'level_select':
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        self.state = 'menu'
-        return
-        
-      max = len(os.listdir(os.path.abspath('levels/')))-1
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-        if self.m_selector == 0:
-          self.m_selector = max
-        else:
-          self.m_selector -= 1
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-        if self.m_selector == max:
-          self.m_selector = 0
-        else:
-          self.m_selector += 1
-          
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-        if self.m_selector == max: #back button pressed
-          self.state = 'menu'
-          self.m_selector = 0
-          return
-          
-        levels = os.listdir(os.path.abspath('levels/'))
-        if 'scores' in levels:
-          levels.remove('scores')
-        self.state = 'playing'     
-        self.time_started = pygame.time.get_ticks()
-        self.pieces_removed = 0
-        self.tiles = load_level(filename=levels[self.m_selector], rnd=True)
-        self.filename = levels[self.m_selector]
-        self.start_piece_count = len(self.tiles)             
-      return
-    elif self.state == 'playing':
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        if not self.editor:
-          self.state = 'paused'
-        else:
-          sys.exit()
-        return
-        
-      self.handle_tile_click(event) 
-    elif self.state == 'paused':
-      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        self.state == 'playing'
-    elif self.state == 'level_complete':
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        self.state = 'level_select'
-        self.selected = None
-          
+    # If our state is valid, it should have an associated input handler,
+    # but as with rendering, check to be sure. :)
+    if self.state in self.input_handlers:
+      self.input_handlers[self.state](event)
+         
   def render(self, screen):
     "Based on the games state, call the appropriate drawing methods"
     # If our state is valid, it should have an associated rendering function
     # specified, but check here just to be sure.
     if self.state in self.render_func: 
       self.render_func[self.state](screen)
+      
+  def handle_menu_input(self, event):
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_DOWN:
+        if self.m_selector == 3:
+          self.m_selector = 0
+        else:
+          self.m_selector += 1
+          
+      if event.key == pygame.K_UP:
+        if self.m_selector == 0:
+          self.m_selector = 3
+        else:
+          self.m_selector -= 1
+        
+      if event.key == pygame.K_RETURN:
+        if self.m_selector == 0:
+          self.state = 'level_select'
+          self.m_selector = 0
+          
+        if self.m_selector == 3:
+          sys.exit()
+        
+      if event.key == pygame.K_ESCAPE:
+        sys.exit()
+          
+  def handle_playing_input(self, event):
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE:
+        if not self.editor:
+          self.state = 'level_select'
+        else:
+          sys.exit()
+        return
+      
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      # Iterate through tiles seing if we've clicked above any of them.
+      # If we have, and it's deemed selectable, then select it. If we have and
+      # A tile is already selected elsewhere, compare the two to see if they're a match.
+      # If they're a match, then remove the tiles and add to our tiles removed tally.
+      for tile in sorted(self.tiles, key=byTopRight, reverse=True) :
+          x,y = event.pos
+          if x >= tile.x - tile.z * 3 and x <= tile.x + 40 - tile.z * 3 and \
+             y >= tile.y - tile.z * 3 and y <= tile.y + 60 - tile.z * 3:
+             if self.selected == tile: # Selecting the selected tile unselects it.
+              self.selected = None
+              return
+             if self.selected: # We clicked on a tile, is a tile already selected?
+              if self.selected.tileno == tile.tileno:     # Remove tiles if they're the same
+                # But only if both tiles are not blocked on left & right
+                if not tile.is_blocked(self.tiles) and not self.selected.is_blocked(self.tiles):
+                  self.tiles.remove(self.selected)
+                  self.tiles.remove(tile)
+                  self.pieces_removed += 2
+                  
+                  # If we won!
+                  if len(self.tiles) == 0:
+                    self.state = 'level_complete'
+                    self.score = str((pygame.time.get_ticks()-self.time_started)/1000)
+                    pygame.event.clear()
+                    
+                    self.write_score()
+                    return
+                self.selected = None
+                return
+              else:
+                self.selected = None
+                return
+             else:
+              if not tile.is_blocked(self.tiles):
+                self.selected = tile       
+             return
+             
+             
+      self.selected = None    
+   
+  def handle_level_select_input(self, event):
+
+    #List levels (as defined by basically any file in the levels/ directory.
+    levels = os.listdir(os.path.abspath('levels/'))
+    max = len(levels)-1 
+    
+    if 'scores' in levels:
+      levels.remove('scores')
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE:
+        self.state = 'menu'
+        return
+      elif event.key == pygame.K_UP:
+        if self.m_selector == 0:
+          self.m_selector = max
+        else:
+          self.m_selector -= 1
+      elif event.key == pygame.K_DOWN:
+        if self.m_selector == max:
+          self.m_selector = 0
+        else:
+          self.m_selector += 1  
+      elif event.key == pygame.K_RETURN:
+      
+        # True if they selected enter while BACK was highlighted.
+        if self.m_selector == max:
+          self.state = 'menu'
+          self.m_selector = 0
+          return
+          
+        self.state = 'playing'     
+        self.time_started = pygame.time.get_ticks()
+        self.pieces_removed = 0
+        self.tiles = load_level(filename=levels[self.m_selector], rnd=True)
+        self.filename = levels[self.m_selector]
+        self.start_piece_count = len(self.tiles)        
+  
+  def handle_level_complete_input(self, event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      self.state = 'level_select'
+      self.selected = None
+  
+  def handle_paused_input(self, event):
+    pass
+    
+  def handle_highscores_input(self, event):
+    pass
   
   def render_highscores(self, screen):
     pygame.draw.rect(screen,(0,0,0), (0,0,800,80))
@@ -257,6 +275,7 @@ class Game:
     render_text(screen, self.font,  str(self.score) + " seconds, sweet!", (20,40,200,200), color=(255,150,122))
       
   def render_playing(self,screen):
+
     render_black_bars(screen)
     
     # True if we're currently in the editor. Just changes the labels
